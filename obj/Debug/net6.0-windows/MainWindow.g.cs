57204539962887e8a62ec546151f9f5b7db2bd9a
Rodrigo Diaz Serviran <rodrigo.diaz@insejupy.gob.mx>
2023-09-18 13:34:08 -0600

    obj

--- obj/Debug/net6.0-windows/MainWindow.g.cs
+++ obj/Debug/net6.0-windows/MainWindow.g.cs
@@ -69,7 +69,7 @@ namespace punto_venta {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/punto_venta;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/punto_venta;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
